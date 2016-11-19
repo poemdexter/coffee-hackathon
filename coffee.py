1f17741459e1e7065c2567977abe3e60e40c542b
Daniel Fairley <poemdexter@gmail.com>
2016-11-19 19:43:57 +0000

    made image save in coffee.py location. added last.jpg to gitignore

--- coffee.py
+++ coffee.py
@@ -6,6 +6,7 @@ from os.path import join, dirname
 from watson_developer_cloud import VisualRecognitionV3
 
 # config setup
+last_image_loc = join(dirname(__file__), 'last.jpg')
 config = {}
 with open(join(dirname(__file__), 'config.json'), 'rb') as data:
   config.update(json.load(data))
@@ -19,22 +20,42 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def take_pic():
   print('Taking Picture')
-  bash_command = "/opt/vc/bin/raspistill -w 300 -h 300 -n -t 1 -q 10 -dt -o /home/pi/Desktop/coffee/new.jpg"
+  bash_command = "/opt/vc/bin/raspistill -w 300 -h 300 -n -t 1 -q 10 -dt -o " + last_image_loc
   process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
   output, error = process.communicate()
+  if error is not None:
+    print(error)
 
-def watson_test():
-  print(json.dumps(visual_recognition.list_classifiers(), indent=2))
-
+def watson_check_debug():
+  print('Watson Image Recognition')
+  with open(last_image_loc, 'rb') as image_file:
+    print(json.dumps(visual_recognition.classify(images_file=image_file, threshold=0.1, classifier_ids=['employees_387877154']), indent=2))
+    
 def watson_check():
   print('Watson Image Recognition')
-  with open(join(dirname(__file__), '../coffee/new.jpg'), 'rb') as image_file:
-    print(json.dumps(visual_recognition.classify(images_file=image_file, threshold=0.1, classifier_ids=['employees_387877154']), indent=2))
+  with open(last_image_loc, 'rb') as image_file:
+    results = visual_recognition.classify(images_file=image_file, threshold=0.1, classifier_ids=['employees_387877154'])
+    
+    # get accused name from results
+    persons = results["images"][0]["classifiers"][0]["classes"]
+    persons.sort(key = lambda x: -x["score"])
+    accused = str(persons[0]["class"])
+    accuracy = str(persons[0]["score"])
+    print('Identified ' + accused + ' (' + accuracy + ')')
+    
+    return accused
+    
+#def upload_to_aws(accused):
+  # todo: upload to aws
 
 while True:
   input_state = GPIO.input(18)
   if input_state == False:
     print('Button Pressed')
     take_pic()
-    watson_check()
-    time.sleep(0.2)
+    
+    accused = watson_check()
+    #watson_check_debug()
+    
+    #upload_to_aws(accused)
+    time.sleep(10)
